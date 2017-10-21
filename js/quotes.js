86836fa79dcf0a61fd79401fd482b04195792e54
Daniel Springer <dssp100@gmail.com>
2017-10-21 20:51:47 +0200

    First part completed
    Added the getRandomQuote function and printQuote function.

---
+++ js/quotes.js
@@ -0,0 +1,32 @@
+var quotes = [
+    {
+        quote: "Transformation is a process, and as life happens there are tons of ups and downs. It's a journey of discovery--there are moments on mountaintops and moments in deep valleys of despair.",
+        source:'Internet',
+        citation:'',
+        year:'1980'
+    },
+    {
+        quote: "Love all, trust a few, do wrong to none.",
+        source:'William Shakespeare',
+        citation:'https://www.values.com/inspirational-quotes?page=2',
+        year:'1950'
+    },
+    {
+        quote: "There is no greater education than one that is self-driven.",
+        source:'Neil deGrasse Tyson',
+        citation:'https://www.values.com/inspirational-quotes?page=2',
+        year:'1950'
+    },
+    {   
+        quote: "Always be yourself and have faith in yourself.",
+        source:'Bruce Lee',
+        citation:'https://www.values.com/inspirational-quotes?page=2',
+        year:'1950'
+    },  
+    {   
+        quote: "Once we believe in ourselves, we can risk curiosity, wonder, spontaneous delight, or any experience that reveals the human spirit.",
+        source:'e. e. cummings',
+        citation:'https://www.values.com/inspirational-quotes?page=2',
+        year:'1950'
+    }
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// creates a randon number between 0 and a number equal to the amount of items in the quotes array (in the quotes.js file)
+// returns a random quote out of the quotes array
+function getRamdomQuote(){
+    var randomNr = Math.floor(Math.random() * quotes.length) 
+    return quotes[randomNr]
+}
+
+// stores the random quote generated in a new variable and and creates a HTML string using the data out of the random quotes from the getRandom quote function.
+// the created string is then send back to the HTML page.
+function printQuote() {
+    var randomQuote = getRamdomQuote()
+    htmlQuoteString = '<p class="quote">' + randomQuote.quote + '</p>' +
+    '<p class="source">' + randomQuote.source +
+        '<span class="citation">' + randomQuote.citation + '</span>'
+        '<span class="year">' + randomQuote.year + '</span>'
+    '</p>'
+   document.getElementById('quote-box').innerHTML = htmlQuoteString
+}
