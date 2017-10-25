19423072643caec37a5867538e17347f7ea1f243
Daniel Springer <dssp100@gmail.com>
2017-10-25 14:09:21 +0200

    Completed the function to not repeat the already shown quote.
    Added functionality to prevent showing a quote twice until the all
    quote have been shown once.
    Cleaned up comments.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
+//var viewedQuotes=[]; // 
+
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-// creates a randon number between 0 and a number equal to the amount of items in the quotes array (in the quotes.js file)
-// returns a random quote out of the quotes array
+// 1. creates a randon number between 0 and a number equal to the amount of items in the quotes array (in the quotes.js file)
+// 2. removes the quote based on the randomly chosen nr from the getRandomQuote function and out of the quotes array (in the quotes.js file) and stores the removed item in a variable called "removedQuote". The .splice() method returns your single object in an array [{}].The [0] is there to access the first element in the single item object array, which becomes the removed quote.
+// 3. the removed quote is then pushed in to a new global array variable called viewedQuotes.
+// 4. the if the checks if the quote is empty and if it is it will "reset" the empty quotes array by setting it equal to the viewedQuotes array 
 
 function getRamdomQuote(){
-    var randomNr = Math.floor(Math.random() * quotes.length) 
-    return quotes[randomNr]
+    var viewedQuotes=[];
+    //1.
+    var randomNr = Math.floor(Math.random() * quotes.length)
+    console.log('This is the random nr ' + randomNr)
+    //2.
+    var removedQuote = quotes.splice(randomNr, 1)[0];
+    console.log(removedQuote)
+    //3.
+    viewedQuotes.push(removedQuote);
+    console.log(viewedQuotes)
+    //4.
+    if (quotes.length === 0) {
+      quotes = viewedQuotes;
+      viewedQuotes = [];
+    }
+    return removedQuote;
 }
 
-// stores the random quote generated in a new variable and and creates a HTML string using the data out of the random quotes provided by the getRandomQuote function.
-// the created string is then send back to the HTML page.
-// change the background color of the page randomly by calling the "changeBackgroundColor()" function.
+
+//1. stores the random quote generated in a new variable and and creates a HTML string using the data out of the random quotes provided by the getRandomQuote function.
+//2. the created string is then send back to the HTML page.
+//3. change the background color of the page randomly by calling the "changeBackgroundColor()" function.
 
 function printQuote() {
-    
+    //1.
     var randomQuote = getRamdomQuote()
     htmlQuoteString = '<p class="quote">' + randomQuote.quote + '</p>' +
     '<p class="source">' + randomQuote.source +
@@ -24,8 +44,11 @@ function printQuote() {
         '<span class="year">' + randomQuote.year + '</span>' +
         '<span class="tag">' + randomQuote.tag + '</span>' +
     '</p>'
+    //2.
     document.getElementById('quote-box').innerHTML = htmlQuoteString
+    //3.
     changeBackgroundColor()
+    
 }
 
 // creates 3 random colors (number between 0 and 255) for red, green, blue and creates an rgbColor string 
@@ -38,6 +61,5 @@ function changeBackgroundColor(){
     document.body.style.background = rgbColor; 
 }
 
-// 
 
 
