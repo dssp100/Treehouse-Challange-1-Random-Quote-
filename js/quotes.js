56e7b1015ee50f47bf72b76c95aebfaf78560664
Daniel Springer <dssp100@gmail.com>
2017-10-25 15:06:35 +0200

    Added time interval for showing new quotes automatically
    Added interval function.
    Improved quotes data.
    Improved comments.

--- js/quotes.js
+++ js/quotes.js
@@ -1,37 +1,37 @@
 var quotes = [
     {
-        quote: "Transformation is a process, and as life happens there are tons of ups and downs. It's a journey of discovery--there are moments on mountaintops and moments in deep valleys of despair.",
-        source:'Internet',
-        citation:'',
-        year:'1980',
-        tag: 'xxx'
+        quote: "Strength and growth come only through continuous effort and struggle.",
+        source:'Napoleon Hill',
+        citation:'https://www.brainyquote.com/quotes/quotes/n/napoleonhi152854.html',
+        year:'1883 - 1970',
+        tag: 'Strenght',
     },
     {
         quote: "Love all, trust a few, do wrong to none.",
         source:'William Shakespeare',
         citation:'https://www.values.com/inspirational-quotes?page=2',
-        year:'1950',
-        tag: 'xxx'
+        year:'1564 - 1616',
+        tag: 'Wisdome'
     },
     {
         quote: "There is no greater education than one that is self-driven.",
         source:'Neil deGrasse Tyson',
         citation:'https://www.values.com/inspirational-quotes?page=2',
-        year:'1950',
-        tag: 'xxx'
+        year:'1958',
+        tag: 'Education'
     },
     {   
         quote: "Always be yourself and have faith in yourself.",
         source:'Bruce Lee',
         citation:'https://www.values.com/inspirational-quotes?page=2',
-        year:'1950',
-        tag: 'xxx'
+        year:'1940 - 1973',
+        tag: 'Faith'
     },  
     {   
-        quote: "Once we believe in ourselves, we can risk curiosity, wonder, spontaneous delight, or any experience that reveals the human spirit.",
-        source:'e. e. cummings',
-        citation:'https://www.values.com/inspirational-quotes?page=2',
-        year:'1950',
-        tag: 'xxx'
+        quote: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.",
+        source:'Francis of Assisi',
+        citation:'https://www.brainyquote.com/quotes/quotes/f/francisofa121023.html',
+        year:'1182 - 1226',
+        tag: 'Inspirational'
     }
 ];
--- js/script.js
+++ js/script.js
@@ -1,42 +1,43 @@
-//var viewedQuotes=[]; // 
+// Global Variables
 
+var viewedQuotes=[];
 
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
+//1. event listener to respond to "Show another quote" button clicks
+//2. when user clicks anywhere on the button, the "printQuote" function is called
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-// 1. creates a randon number between 0 and a number equal to the amount of items in the quotes array (in the quotes.js file)
-// 2. removes the quote based on the randomly chosen nr from the getRandomQuote function and out of the quotes array (in the quotes.js file) and stores the removed item in a variable called "removedQuote". The .splice() method returns your single object in an array [{}].The [0] is there to access the first element in the single item object array, which becomes the removed quote.
-// 3. the removed quote is then pushed in to a new global array variable called viewedQuotes.
-// 4. the if the checks if the quote is empty and if it is it will "reset" the empty quotes array by setting it equal to the viewedQuotes array 
+// 3. creates a randon number between 0 and a number equal to the amount of items in the quotes array (in the quotes.js file)
+// 4. removes the quote based on the randomly chosen nr from the getRandomQuote function and out of the quotes array (in the quotes.js file) and stores the removed item in a variable called "removedQuote". The .splice() method returns your single object in an array [{}].The [0] is there to access the first element in the single item object array, which becomes the removed quote.
+// 5. the removed quote is then pushed in to a new global array variable called viewedQuotes.
+// 6. the if the checks if the quotes array is empty and if it is it will "reset" the empty quotes array by setting it equal to the viewedQuotes array. The viewedQuotes array is then emptied by using [].
+// 7. returns the removed quote.
 
 function getRamdomQuote(){
-    var viewedQuotes=[];
-    //1.
+    //3.
     var randomNr = Math.floor(Math.random() * quotes.length)
     console.log('This is the random nr ' + randomNr)
-    //2.
+    //4.
     var removedQuote = quotes.splice(randomNr, 1)[0];
     console.log(removedQuote)
-    //3.
+    //5.
     viewedQuotes.push(removedQuote);
     console.log(viewedQuotes)
-    //4.
+    //6.
     if (quotes.length === 0) {
       quotes = viewedQuotes;
       viewedQuotes = [];
     }
+    //7.
     return removedQuote;
 }
 
-
-//1. stores the random quote generated in a new variable and and creates a HTML string using the data out of the random quotes provided by the getRandomQuote function.
-//2. the created string is then send back to the HTML page.
-//3. change the background color of the page randomly by calling the "changeBackgroundColor()" function.
+//8. stores the random quote generated in a new variable and creates an HTML string using the data out of the random quotes provided by the getRandomQuote function.
+//9. the created string is then send back to the HTML page.
+//10. change the background color of the page randomly by calling the "changeBackgroundColor()" function.
 
 function printQuote() {
-    //1.
+    //8.
     var randomQuote = getRamdomQuote()
     htmlQuoteString = '<p class="quote">' + randomQuote.quote + '</p>' +
     '<p class="source">' + randomQuote.source +
@@ -44,14 +45,13 @@ function printQuote() {
         '<span class="year">' + randomQuote.year + '</span>' +
         '<span class="tag">' + randomQuote.tag + '</span>' +
     '</p>'
-    //2.
+    //9.
     document.getElementById('quote-box').innerHTML = htmlQuoteString
-    //3.
+    //10.
     changeBackgroundColor()
-    
 }
 
-// creates 3 random colors (number between 0 and 255) for red, green, blue and creates an rgbColor string 
+//11. creates 3 random colors (number between 0 and 255) for red, green, blue and creates an rgbColor string which is send to the css
 
 function changeBackgroundColor(){
     var red = Math.floor(Math.random() * 256 );
@@ -61,5 +61,9 @@ function changeBackgroundColor(){
     document.body.style.background = rgbColor; 
 }
 
+//12. Set a time interval to reload quotes (Call the printQuote function) every 5 seconds.
+
+setInterval(printQuote, 5000);
 
 
+// Daniel Springer, completed 25.Oct 2017
